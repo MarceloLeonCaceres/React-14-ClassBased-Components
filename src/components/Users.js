e2481fb9a596d77c2605c5b545f0ad1800cfe2e5
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2023-12-26 12:36:23 -0500

    288 Introducing Error Boundaries

--- src/components/Users.js
+++ src/components/Users.js
@@ -12,6 +12,13 @@ class Users extends Component {
       more: "Test",
     };
   }
+
+  componentDidUpdate(){
+    if(this.props.users.length === 0) {
+      throw new Error('No users provided!');
+    }
+  }
+
   toggleUsersHandler() {
     // this.state.showUsers = false; // NOT!
     this.setState((curState) => {
